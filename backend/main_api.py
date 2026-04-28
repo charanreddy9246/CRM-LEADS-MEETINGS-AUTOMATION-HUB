e7aa5a5ffe1c73f7eb69fedd34d7ab1ab2f9ed44
charanreddy9246 <charanreddy.9246@gmail.com>
2026-04-28 21:37:11 +0530

    modified backned

--- backend/main_api.py
+++ backend/main_api.py
@@ -135,9 +135,16 @@ def get_env_var(key):
     return os.getenv(key)
 
 app = FastAPI()
+origins = [
+    "https://crm-pipeline-leads-meetings-automati.netlify.app",
+    "http://localhost:5173",
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
